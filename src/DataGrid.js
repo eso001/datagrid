644cedb29b8a9b0378c66cf3f731fe588c4e4d2f
Eric <ericso0303@gmail.com>
2017-12-15 14:12:49 -0800

    Completed data grid with dragable and sortable columns

--- src/DataGrid.js
+++ src/DataGrid.js
@@ -1,111 +1,106 @@
 
-const ReactDataGrid = require('react-data-grid');
-const React = require('react');
-const { Toolbar, Data: { Selectors } } = require('react-data-grid-addons');
+import ReactDataGrid from 'react-data-grid'
+import React, {Component} from 'react'
 
-export default class Example extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this._columns = [
+export default class DataGrid extends Component { 
+    constructor(props) {
+      super(props)
+      const originalRows = this.createRows()
+      const rows = [...originalRows]
+      this.state = {originalRows, rows}
+
+    }
+    _columns = [
       {
         key: 'id',
         name: 'ID',
-        width: 80,
-        filterable: true
+        resizable: true,
+        sortable: true,
+        width: 40
       },
       {
         key: 'task',
         name: 'Title',
-        filterable: true
+        sortable: true,        
+        resizable: true
       },
       {
         key: 'priority',
         name: 'Priority',
-        filterable: true
+        sortable: true,
+        resizable: true
       },
       {
         key: 'issueType',
         name: 'Issue Type',
-        filterable: true
+        sortable: true,
+        resizable: true
       },
       {
         key: 'complete',
         name: '% Complete',
-        filterable: true
+        sortable: true,
+        resizable: true
       },
       {
         key: 'startDate',
         name: 'Start Date',
-        filterable: true
+        sortable: true,
+        resizable: true
       },
       {
         key: 'completeDate',
         name: 'Expected Complete',
-        filterable: true
+        sortable: true,
+        resizable: true
       }
     ];
 
-    this.state = { rows: this.createRows(), filters: {} };
-  }
-
-  getRandomDate = (start, end) => {
-    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toLocaleDateString();
-  };
-
-  createRows = () => {
-    let rows = [];
-    for (let i = 1; i < 1000; i++) {
-      rows.push({
-        id: i,
-        task: 'Task ' + i,
-        complete: Math.min(100, Math.round(Math.random() * 110)),
-        priority: ['Critical', 'High', 'Medium', 'Low'][Math.floor((Math.random() * 3) + 1)],
-        issueType: ['Bug', 'Improvement', 'Epic', 'Story'][Math.floor((Math.random() * 3) + 1)],
-        startDate: this.getRandomDate(new Date(2015, 3, 1), new Date()),
-        completeDate: this.getRandomDate(new Date(), new Date(2016, 0, 1))
-      });
+    handleGridSort = (sortColumn, sortDirection) => {
+      const comparer = (a, b) => {
+        if (sortDirection === 'ASC') {
+          return (a[sortColumn] > b[sortColumn]) ? 1 : -1;
+        } else if (sortDirection === 'DESC') {
+          return (a[sortColumn] < b[sortColumn]) ? 1 : -1;
+        }
+      };
+  
+      const rows = sortDirection === 'NONE' ? this.state.originalRows.slice(0) : this.state.rows.sort(comparer);
+  
+      this.setState({ rows });
     }
-
-    return rows;
-  };
-
-  getRows = () => {
-    return Selectors.getRows(this.state);
-  };
-
-  getSize = () => {
-    return this.getRows().length;
-  };
-
-  rowGetter = (rowIdx) => {
-    let rows = this.getRows();
-    return rows[rowIdx];
-  };
-
-  handleFilterChange = (filter) => {
-    let newFilters = Object.assign({}, this.state.filters);
-    if (filter.filterTerm) {
-      newFilters[filter.column.key] = filter;
-    } else {
-      delete newFilters[filter.column.key];
+    getRandomDate = (start, end) => {
+      return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toLocaleDateString();
     }
-    this.setState({ filters: newFilters });
-  };
-
-  onClearFilters = () => {
-    // all filters removed
-    this.setState({filters: {} });
-  };
-
-  render() {
-    return (
-      <ReactDataGrid
-        columns={this._columns}
-        rowGetter={this.rowGetter}
-        enableCellSelect={true}
-        rowsCount={this.getSize()}
-        minHeight={500}
-        onAddFilter={this.handleFilterChange}
-        onClearFilters={this.onClearFilters} />);
-  }
+  
+    createRows = () => {
+      let rows = [];
+      for (let i = 1; i < 20000; i++) {
+        rows.push({
+          id: i,
+          task: 'Task ' + i,
+          complete: Math.min(100, Math.round(Math.random() * 110)),
+          priority: ['Critical', 'High', 'Medium', 'Low'][Math.floor((Math.random() * 3) + 1)],
+          issueType: ['Bug', 'Improvement', 'Epic', 'Story'][Math.floor((Math.random() * 3) + 1)],
+          startDate: this.getRandomDate(new Date(2015, 3, 1), new Date()),
+          completeDate: this.getRandomDate(new Date(), new Date(2016, 0, 1))
+        });
+      }
+  
+      return rows;
+    }
+  
+    rowGetter = (i) => {
+      return this.state.rows[i];
+    }
+  
+    render = () => {
+      return  (
+        <ReactDataGrid
+          columns={this._columns}
+          onGridSort={this.handleGridSort}
+          rowGetter={this.rowGetter}
+          rowsCount={this.state.rows.length}
+          minHeight={500} />);
+    }
 }
