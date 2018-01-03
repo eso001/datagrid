8c70e893f729fcf75b3294505a495e3326fdc10e
Eric So <eso@paciolan.com>
2018-01-03 11:05:16 -0800

    minor changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <p className="App-intro">
           <Example />
+        </p>
       </div>
     );
   }
